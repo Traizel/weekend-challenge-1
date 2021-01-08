3c8e7aed1f61e01a6a68aa7e3d4ea684643ee919
Tre Olson <treolson@live.com>
2021-01-08 16:57:16 -0600

    started main goals

---
+++ client.js
@@ -0,0 +1,30 @@
+$(document).ready(onReady);
+
+function onReady() {
+    $('#add-employee').on('click', function() {
+        let firstName = $('#first-name').val();
+        let lastName = $('#last-name').val();
+        let id = $('#id').val();
+        let title = $('#title').val();
+        let anualSalary = $('#anual-salary').val();
+        $('#employee-table').append(`
+            <tr>
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${id}</td>
+                <td>${title}</td>
+                <td>${anualSalary}</td>
+            </tr>
+        `);
+        $('.collection').val('');
+        anualSalary = Number(anualSalary);
+        let totalMonthlyCosts = $('#monthly-costs').text();
+        totalMonthlyCosts = Number(totalMonthlyCosts);
+        totalMonthlyCosts += anualSalary;
+        $('#monthly-costs').text(totalMonthlyCosts);
+
+        if (totalMonthlyCosts > 20000) {
+            $('footer h2').css('background-color', '#EF626C');
+        }
+    });
+}
