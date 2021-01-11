c5126056e60c19a014da5360d02268d699796c29
Tre Olson <treolson@live.com>
2021-01-11 12:01:19 -0600

    added comment code

--- client.js
+++ client.js
@@ -2,11 +2,13 @@ $(document).ready(onReady);
 
 function onReady() {
     $('#add-employee').on('click', function() {
+        // assign employee info to variables
         let firstName = $('#first-name').val();
         let lastName = $('#last-name').val();
         let id = $('#id').val();
         let title = $('#title').val();
         let anualSalary = $('#anual-salary').val();
+        // check if there are no inputs in form
         if (firstName === '' || lastName === '' || title === '') {
             alert('You must provide all inputs!');
             return 0;
@@ -14,6 +16,7 @@ function onReady() {
             alert('You must provide numbers greater than 0!')
             return 0;
         }
+        // add employee info to table
         $('#employee-table tbody').append(`
             <tr>
                 <td>${firstName}</td>
@@ -24,17 +27,19 @@ function onReady() {
                 <td><button class="delete-employee">Delete</button></td>
             </tr>
         `);
+        // add anual salary to total monthly costs
         $('.collection').val('');
         anualSalary = Number(anualSalary);
         let totalMonthlyCosts = $('#monthly-costs').text();
         totalMonthlyCosts = Number(totalMonthlyCosts);
         totalMonthlyCosts += (anualSalary / 12);
         $('#monthly-costs').text(totalMonthlyCosts.toFixed(2));
-
+        // check if total costs if over 20k
         if (totalMonthlyCosts > 20000) {
             $('footer h2').addClass('over-budget');
         }
     });
+    // delete button
     $('#employee-table').on('click', '.delete-employee', function() {
         let removeSalary = $(this).parent().siblings('.salary').text();
         removeSalary = Number(removeSalary);
